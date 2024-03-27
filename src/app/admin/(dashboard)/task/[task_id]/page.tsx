0fc5a273444ec43100fd40d67ecdb55d7807853b
Raphael Brandão Mesquita <raphaelbmesquita@gmail.com>
2024-03-27 09:30:39 +0000

    fix: small fixes

--- src/app/admin/(dashboard)/task/[task_id]/page.tsx
+++ src/app/admin/(dashboard)/task/[task_id]/page.tsx
@@ -1,16 +1,30 @@
-//fetch
-import { getRosterTask } from "@/common/libs/company/actions/rosterTask"
+'use client'
+
+//components
 import FormContainer from "./_components/formContainer"
 
+//hooks
+import { useGETCompanyDataHooks } from "@/hooks/company/companyDataHooks"
 
-export default async function Task({ params }: { params: { task_id: string } }) {
-    const data = await getRosterTask({
-        rosterTask: {
-            id: params.task_id
+
+export default function Task({ params }: { params: { task_id: string } }) {
+    const {
+        companyRosterTask: rosterTask,
+    } = useGETCompanyDataHooks({
+        query: 'ROSTER_TASKS',
+        defaultParams: {
+            roster: {
+                rosterTask: {
+                    id: params.task_id
+                }
+            }
+        },
+        UseQueryOptions: {
+            enabled: !!params.task_id
         }
     })
 
     return (
-        <FormContainer form={data?.form} rosterTask={data?.task} />
+        <FormContainer form={rosterTask?.form} rosterTask={rosterTask?.task} />
     )
 }
