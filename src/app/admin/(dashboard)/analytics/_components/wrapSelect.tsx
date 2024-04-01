7084deb0f30ddbcae4b2a60303b5ba84360224c0
Raphael Brandão Mesquita <raphaelbmesquita@gmail.com>
2024-04-01 17:15:55 +0100

    fix: download sales

--- src/app/admin/(dashboard)/analytics/_components/wrapSelect.tsx
+++ src/app/admin/(dashboard)/analytics/_components/wrapSelect.tsx
@@ -1,35 +1,19 @@
 'use client'
-import Icon from "@/common/libs/lucida-icon";
 import { cn } from "@/common/libs/shadcn/utils";
-import { Button } from "@/components/ui/button";
 
 interface WrapSelectProps {
     children: React.ReactNode
     selected: boolean
     handleSelect: () => void
-    donwload?: {
-        onClick: () => void
-    }
 }
 
-export default function WrapSelect({ children, selected, handleSelect, donwload }: WrapSelectProps): JSX.Element {
+export default function WrapSelect({ children, selected, handleSelect }: WrapSelectProps): JSX.Element {
     return (
         <div
             className={cn('border-2 rounded-2xl cursor-pointer w-full relative', selected ? 'border-primary' : 'border-transparent')}
             onClick={handleSelect}
         >
-            {donwload &&
-                <Button
-                    className='absolute left-2 top-2'
-                    size='iconExSm'
-                    variant='orange'
-                    onClick={donwload.onClick}
-                >
-                    <Icon
-                        name='FileDown'
-                    />
-                </Button>
-            }
+           
             {children}
         </div>
     )
